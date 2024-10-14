4dac5111a8fcf2de6ee5609095d48d5afb988cb2
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-10-14 16:37:07 +0700

    Make Some Resource

---
+++ app/Filament/Resources/PemesananResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PemesananResource\Pages;
+use App\Filament\Resources\PemesananResource\RelationManagers;
+use App\Models\Pemesanan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PemesananResource extends Resource
+{
+    protected static ?string $model = Pemesanan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('mobil_id')
+                    ->relationship('mobil', 'id')
+                    ->required(),
+                Forms\Components\DatePicker::make('tanggal_mulai')
+                    ->required(),
+                Forms\Components\DatePicker::make('tanggal_selesai')
+                    ->required(),
+                Forms\Components\TextInput::make('total_harga')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('mobil.id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('tanggal_mulai')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('tanggal_selesai')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_harga')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPemesanans::route('/'),
+            'create' => Pages\CreatePemesanan::route('/create'),
+            'edit' => Pages\EditPemesanan::route('/{record}/edit'),
+        ];
+    }
+}
